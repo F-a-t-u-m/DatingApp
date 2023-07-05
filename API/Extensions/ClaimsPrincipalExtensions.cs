c4f8e8765a9f9bd41d99de3a266bcb995ad85893
F-a-t-u-m <pavlo.perehinets.kb.2020@lpnu.ua>
2023-07-05 13:59:07 +0300

    End of section 14

--- API/Extensions/ClaimsPrincipalExtensions.cs
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -8,9 +8,9 @@ namespace API.Extensions
 		{
 			return user.FindFirst(ClaimTypes.Name)?.Value;
 		}
-		public static string GetUserId(this ClaimsPrincipal user) //need int
+		public static int GetUserId(this ClaimsPrincipal user)
 		{
-			return user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+			return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
 		}
 	}
 }
