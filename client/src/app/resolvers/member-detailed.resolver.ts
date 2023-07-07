2aa7cc09467ad4c3f859482a882a24c09e2227d1
F-a-t-u-m <pavlo.perehinets.kb.2020@lpnu.ua>
2023-07-07 20:08:25 +0300

    End of section 15

---
+++ client/src/app/resolvers/member-detailed.resolver.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+import { MembersService } from '../services/members.service';
+import { Member } from '../models/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberDetailedResolver implements Resolve<Member> {
+  constructor(
+    private memberService: MembersService
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<Member> {
+    return this.memberService.getMember(route.paramMap.get('username')!)
+  }
+}
